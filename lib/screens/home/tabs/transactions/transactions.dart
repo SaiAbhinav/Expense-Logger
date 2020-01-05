225f68c19b3b5e8d955afb675990985425be8326
sai abhinav <saiabhinavvasepalli@gmail.com>
2020-01-05 23:51:00 +0530

    added date range

--- lib/screens/home/tabs/transactions/transactions.dart
+++ lib/screens/home/tabs/transactions/transactions.dart
@@ -5,6 +5,7 @@ import 'package:expense_logger/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
+import 'package:date_range_picker/date_range_picker.dart' as DateRagePicker;
 
 class Transactions extends StatefulWidget {
   @override
@@ -19,6 +20,9 @@ class _TransactionsState extends State<Transactions> {
     var formatter = new DateFormat('EEE, MMM dd, yyyy');
     return formatter.format(dateTimeObj);
   }
+
+  bool _pickedRange = false;
+  List<DateTime> _pickedDatesRange = [];
 
   @override
   Widget build(BuildContext context) {
@@ -47,10 +51,8 @@ class _TransactionsState extends State<Transactions> {
       return total.toString();
     }
 
-    List<Widget> _getChildren() {
-      List<String> sortedKeys = List<String>.from(transactions.data['transactions'].keys);
-      sortedKeys.sort((b, a) => a.compareTo(b));      
-      return sortedKeys.map<Widget>((key) {
+    List<Widget> _getChildren(List<String> keys) {
+      return keys.map<Widget>((key) {
         return Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
@@ -90,22 +92,98 @@ class _TransactionsState extends State<Transactions> {
       }).toList();
     }
 
+    List<Widget> _getChildrenInRange() {
+      List<String> sortedKeys =
+          List<String>.from(transactions.data['transactions'].keys);
+      sortedKeys.sort();
+      List<String> stringPickedDates = [];
+      _pickedDatesRange.forEach((date) {
+        stringPickedDates.add(date.toString());
+      });
+      List<String> pickedRangeDates = [];
+      stringPickedDates.forEach((date) {
+        String dateString = date.split(' ')[0];
+        List<String> splitedDate = dateString.split('-');
+        pickedRangeDates
+            .add('${splitedDate[2]}_${splitedDate[1]}_${splitedDate[0]}');
+      });
+      int startIndex = sortedKeys.indexOf(pickedRangeDates[0]) != -1
+          ? sortedKeys.indexOf(pickedRangeDates[0])
+          : 0;
+      int endIndex = sortedKeys.indexOf(pickedRangeDates[1]) != -1
+          ? (sortedKeys.indexOf(pickedRangeDates[1]) + 1)
+          : sortedKeys.length;
+      print(sortedKeys.sublist(startIndex, endIndex));
+      List<String> subList = sortedKeys.sublist(startIndex, endIndex);
+      subList.sort((b, a) => a.compareTo(b));
+      return _getChildren(subList);
+    }
+
+    List<Widget> _getAllChildren() {
+      List<String> sortedKeys =
+          List<String>.from(transactions.data['transactions'].keys);
+      sortedKeys.sort((b, a) => a.compareTo(b));
+      return _getChildren(sortedKeys);
+    }
+
     return Scaffold(
       appBar: AppBar(
         elevation: 0.0,
         backgroundColor: Colors.white,
         actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: IconButton(
-              icon: Icon(
-                Icons.date_range,
-                color: Colors.black38,
-                size: 30.0,
-              ),
-              onPressed: () {},
-            ),
-          )
+          _pickedRange
+              ? Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: FlatButton.icon(
+                    splashColor: Colors.transparent,
+                    label: Text(
+                      'Clear range',
+                      style: TextStyle(color: Colors.black38),
+                    ),
+                    color: Colors.white,
+                    icon: Icon(
+                      Icons.cancel,
+                      color: Colors.red,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _pickedRange = false;
+                      });
+                    },
+                  ),
+                )
+              : Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: FlatButton.icon(
+                    splashColor: Colors.transparent,
+                    label: Text(
+                      'Select range',
+                      style: TextStyle(color: Colors.black38),
+                    ),
+                    icon: Icon(
+                      Icons.date_range,
+                      color: Colors.black38,
+                      size: 30.0,
+                    ),
+                    onPressed: () async {
+                      final List<DateTime> picked =
+                          await DateRagePicker.showDatePicker(
+                        context: context,
+                        initialFirstDate: DateTime.now(),
+                        initialLastDate: DateTime.now(),
+                        firstDate: DateTime(2019, 1, 1),
+                        lastDate: DateTime(DateTime.now().year,
+                            DateTime.now().month, DateTime.now().day + 1),
+                      );
+                      if (picked != null && picked.length > 1) {
+                        setState(() {
+                          _pickedRange = true;
+                          _pickedDatesRange = picked;
+                        });
+                      }
+                    },
+                  ),
+                ),
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
@@ -132,7 +210,10 @@ class _TransactionsState extends State<Transactions> {
                   width: double.infinity,
                   height: double.infinity,
                   // padding: EdgeInsets.only(top: 20.0),
-                  child: ListView(children: _getChildren()),
+                  child: ListView(
+                      children: _pickedRange
+                          ? _getChildrenInRange()
+                          : _getAllChildren()),
                 )
               : Container(
                   child: Center(
